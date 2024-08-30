33c23765860930ada0dec32f7f9f3fd00661a0c8
Zoltan Turi <turizoltan@sbagroupzrt.onmicrosoft.com>
2024-08-30 10:55:04 +0200

    deploy

---
+++ docker-compose-up-stg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Determine the host IP address
+HOST_IP=$(ip addr show docker0 | grep 'inet ' | awk '{ print $2 }' | cut -d/ -f1)
+
+# Export the HOST_IP variable for Docker Compose to use
+export HOST_IP
+
+# Run Docker Compose with the extra_hosts setting
+docker-compose down --remove-orphans &&
+docker-compose build --no-cache &&
+docker-compose up -d
